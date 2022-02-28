92a8696d8998c8036fa2d681ff864873bf5cc281
Dima <dim_buldakov@mail.ru>
2022-02-28 21:27:24 +0300

    Commit 7

--- KR/settings.py
+++ KR/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-db_from_env = dj-database-url.config()
-DATABASE['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
